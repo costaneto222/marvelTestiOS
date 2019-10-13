b40041b53217af918ff44c4a385f3d47ddccd7e8
costaneto222 <costaneto222@gmail.com>
2019-10-13 02:33:12 -0300

    intiating carousel for comics in detail screen

---
+++ TesteiOS_Marvel/Sections/CharsDetails/Views/ComicsCarousel.swift
@@ -0,0 +1,21 @@
+//
+//  ComicsCarousel.swift
+//  TesteiOS_Marvel
+//
+//  Created by Francisco Costa Neto on 13/10/19.
+//  Copyright © 2019 Francisco Costa Neto. All rights reserved.
+//
+
+import UIKit
+
+class ComicsCarousel: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
