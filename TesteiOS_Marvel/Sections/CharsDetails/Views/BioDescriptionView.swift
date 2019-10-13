46f041c1641c1e7a97d0cb4653e5d8ceda369ff7
costaneto222 <costaneto222@gmail.com>
2019-10-13 01:52:29 -0300

    some aditional fix for details view controller

---
+++ TesteiOS_Marvel/Sections/CharsDetails/Views/BioDescriptionView.swift
@@ -0,0 +1,9 @@
+//
+//  BioDescriptionView.swift
+//  TesteiOS_Marvel
+//
+//  Created by Francisco Costa Neto on 13/10/19.
+//  Copyright © 2019 Francisco Costa Neto. All rights reserved.
+//
+
+import Foundation
